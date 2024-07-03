0c658609134cef771d6ecbb3626af8705e583427
anster <shrey.khet@gmail.com>
2024-07-03 08:18:35 +0530

    Adding Java Interview Questions

---
+++ Java/SumOfTwoIntegers.java
@@ -0,0 +1,14 @@
+// Question : Write a program that gives us the result for adding 2 numbers
+
+import java.utils.*;
+import java.io.*;
+import java.lang.*;
+
+class Main{
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 5;
+		int c = a+b;
+		System.out.println("Sum of Two numbers is : "+ c);
+	}
+}
