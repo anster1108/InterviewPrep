5a76590554288091a2f9843b0f0fdbd44a717134
anster <shrey.khet@gmail.com>
2024-07-29 13:28:51 +0530

    Arrays Rearrange

---
+++ Java/DSA/Arrays/Hard/ParcalTriangle.java
@@ -0,0 +1,35 @@
+/*
+
+ParcalTriangle : 
+							| Rows
+	      1 				| 1	
+	   1  2   1				| 2
+    1   3   3   1			| 3	
+  1  4   6   4   1 			| 4
+1   5  10   10  5   1 		| 5
+
+
+There are 3 basic question from this Pascal triangle 
+
+Q1. Given the row and column number print the element
+	Row = 5 , Column : 3 
+	O/P: 6
+Q2. Print the given row, where row is given 
+	Row :  5
+	O/P: 1   5  10   10  5   1
+
+Q3. Print the whole pascal triagnle tillgiven row
+	Row : 3
+	0/P : 
+							| Rows
+	      1 				| 1	
+	   1  2   1				| 2
+    1   3   3   1			| 3	
+
+*/
+
+class ParcalTriangle{
+	public static void main(String[] args) {
+		
+	}
+}
