9b0624595767a2d8ed2740f42ab9ad62b06876ff
anster1108 <shrey.khet@gmail.com>
2025-01-27 13:50:31 +0530

    Medium BST

---
+++ Java/DSA/Concepts/Binary Search Tree/Medium/InsertNewNode.java
@@ -0,0 +1,73 @@
+/*
+	Insert a given node in BST
+		Given the root node of a binary search tree (BST) and a value val to insert into the tree.
+
+		Return the root node of the BST after the insertion.
+		It is guaranteed that the new value does not exist in the original BST.
+*/
+
+public class TreeNode {
+    int data;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int val) { data = val; left = null; right = null; }
+}
+
+class InsertNewNode {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        // If the root is null, create a new node with the given value and return it
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        
+        // Traverse the tree to find the correct insertion point
+        TreeNode current = root;
+        while (true) {
+            if (val < current.data) {
+                // Move to the left subtree
+                if (current.left == null) {
+                    current.left = new TreeNode(val);
+                    break;
+                } else {
+                    current = current.left;
+                }
+            } else {
+                // Move to the right subtree
+                if (current.right == null) {
+                    current.right = new TreeNode(val);
+                    break;
+                } else {
+                    current = current.right;
+                }
+            }
+        }
+        
+        // Return the modified tree's root node
+        return root;
+    }
+    
+    // Helper function to print the tree in-order
+    private void printInOrder(TreeNode root) {
+        if (root == null) return;
+        printInOrder(root.left);
+        System.out.print(root.data + " ");
+        printInOrder(root.right);
+    }
+    
+    public static void main(String[] args) {
+        InsertNewNode InsertNewNode = new InsertNewNode();
+        
+        // Create a sample BST: [4, 2, 7, 1, 3]
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        
+        int val = 5;
+        root = InsertNewNode.insertIntoBST(root, val);
+        
+        // Print the BST in-order to verify the insertion
+        InsertNewNode.printInOrder(root); // Output should be: 1 2 3 4 5 7
+    }
+}
